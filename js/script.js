62f4502880975fa00d6a43924c16c46e5df38b0e
Toni <tpenya@intelligentpharma.com>
2017-01-04 17:15:10 +0100

    increase loudness on avatar click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var LOUDNESS_INCREMENT = 5;
+
 $( document ).ready(function() {
     retrieveHowards(fillHowardView);
 });
@@ -6,13 +8,15 @@ function fillHowardView(howards) {
     var ul = $('<ul/>');
     howards.forEach(function(howard, idx) {
         var howardColor = getColor(howards.length, idx);
+        howard.id = idx;
         var li = $('<li/>')
             .attr('id', 'howard-' + idx)
             .appendTo(ul);
         $('<span/>')
             .addClass('avatar')
             .css("background-color", getColor(howards.length, idx))
-            .appendTo(li);
+            .appendTo(li)
+            .click(howard, increaseLoudness);
         $('<span/>')
             .addClass('name')
             .text(howard.name)
@@ -31,6 +35,25 @@ function fillHowardView(howards) {
     $('#loud-list').append(ul);
 }
 
+function increaseLoudness(event) {
+    var howard = event.data;
+    howard.loudness += 5;
+    if (howard.loudness > 100) {
+        howard.loudness = 100;
+    };
+
+    console.log(howard);
+
+    $.ajax({
+        method: 'PUT',
+        url: 'https://loud-o-meter.firebaseio.com/howards/' + howard.id + '.json',
+        data: JSON.stringify(howard),
+        success: function(howard) {
+            $('#howard-' + howard.id + ' .progress').animate({width: howard.loudness + '%'}, 1000);
+        }
+    });
+}
+
 function getColor(numberOfPartitions, index) {
     var hue = 360/numberOfPartitions*index;
     return 'hsl(' + hue + ', 72%, 56%)';
@@ -38,9 +61,8 @@ function getColor(numberOfPartitions, index) {
 
 function retrieveHowards(callback) {
     $.ajax({
-        url: "https://loud-o-meter.firebaseio.com/howards.json",
-        jsonp: "callback",
-        dataType: "jsonp",
+        url: 'https://loud-o-meter.firebaseio.com/howards.json',
+        dataType: 'jsonp',
         success: callback
     });
 }
